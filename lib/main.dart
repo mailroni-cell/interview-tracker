ebc371bb48d1f143a153a864fb2a54cde3fa53ed
mailroni-cell <mail.roni@gmail.com>
2026-09-25 00:34:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class InterviewApp extends StatelessWidget {
       ),
       builder: (context, child) {
         return Directionality(
-          textDirection: TextDirection.rtl,
+          textDirection: TextDirection.RTL,
           child: child!,
         );
       },
